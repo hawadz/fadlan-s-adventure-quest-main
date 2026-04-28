a722502430cdd2b016404b1fe038b6b395cd530b
hawadz <hawarizmiummul11@gmail.com>
2026-04-28 23:38:40 +0700

    setup vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  vite: {
-    base: "/fadlan-s-adventure-quest-main/"
-  }
-});
+export default defineConfig();
